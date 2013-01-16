f420a8c43649338ade037bf48c586fb8a3cda82b
Uzair Bukhari <uahmed@folio3.com>
2013-01-16 13:21:40 +0500

    Build One

---
+++ JavaScripts/App.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+
+    $("#tabs").tabs({
+        load: function () {
+        }
+    }),
+
+    $("#btnNext").bind('click', function () {
+        $("#tabs").tabs('select', $("#tabs").tabs("option", "selected") + 1);
+    });
+
+    $("#btnPrv").bind('click', function () {
+        $("#tabs").tabs('select', $("#tabs").tabs("option", "selected") - 1);
+    });
+
+    $("#btnLoad").bind('click', createSummary);
+
+    $('#btnAddInterest').click(addInterest);
+
+    //$("#basicInfo").load("../Pages/basicInfo.htm");
+
+    $('input:radio').bind('click', loadCustomTab);
+
+    bindValidator(); // Bind the validator to all the input controls
+});
+
+var addInterest = function () {
+    var text = $('#txtInterest').val();
+    if (text != '')
+        $('#interestList').append('<li>' + text + '</li>');
+    $('#txtInterest').val('').focus();
+}
+
+var isProLoad = false, isStdLoad = false;
+
+var loadCustomTab = function () {
+    var a = $('#rdStd').is(':checked');
+    //alert(a);
+    if (a && !isStdLoad) {
+        $("#tabs-3").load("../Pages/StudentForm.txt");
+        isStdLoad = true;
+        isProLoad = false;
+    }
+    else if (!a && !isProLoad) {
+        isProLoad = true;
+        isStdLoad = false;
+        $("#tabs-3").load("../Pages/ProfessionalForm.txt");
+    }
+}
+
+var createSummary = function () {
+    $('#summary').empty().append('<table>');
+    var elements = $('input:text');
+    for (var i = 0; i < elements.length; i++) {
+        var a = elements[i];
+        $('#summary').append('<tr><td>' + a.name + '</td><td> ' + a.value + '</td></tr>');
+    }
+    $('#summary').append('</table>');
+    alert(elements.length);
+}
+
+var bindValidator = function () {
+    var elements = $('input:text');
+    for (var i = 0; i < elements.length; i++) {
+        (function (index) {
+            var element = elements[index];
+            element.onkeyup = function () {
+                namespace.Util.MyValidatorClass.FormValidator(element);
+            }
+        })(i);
+    }
+}
---
+++ JavaScripts/MyValidator.js
@@ -0,0 +1,68 @@
+﻿
+var namespace = {};
+namespace.Util = {};
+
+namespace.Util.MyValidatorClass = (function () {
+    var requiredField_Validation = function (text) {
+        var reg = new RegExp('^[\s]*[\s]*$');
+        return !reg.test(text);
+    }
+    var numeric_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp('^\\d*$');
+        return reg.test(text);
+    }
+    var alphaNumeric_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp('^[a-zA-Z0-9_]*$');
+        return reg.test(text);
+    }
+    var email_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        return reg.test(text);
+    }
+    var FormValidator_Onclick = function () {
+        var elements = document.forms[0].elements;
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (element.type == 'text') {
+                namespace.Util.MyValidatorClass.FormValidator(element);
+            }
+        }
+    }
+
+    var Form_Validator = function (Element) {
+        switch (Element.getAttribute("validationtype")) {
+            case 'required':
+                namespace.Util.Design.SetColorOnvalidation(requiredField_Validation(Element.value), Element);
+                break;
+            case 'numeric':
+                namespace.Util.Design.SetColorOnvalidation(numeric_Validation(Element.value), Element);
+                break;
+            case 'email':
+                namespace.Util.Design.SetColorOnvalidation(email_Validation(Element.value), Element);
+                break;
+            case 'alphanumeric':
+                namespace.Util.Design.SetColorOnvalidation(alphaNumeric_Validation(Element.value), Element);
+                break;
+            case 'gpa':
+                namespace.Util.Design.SetColorOnvalidation(GPA_Validation(Element.value), Element);
+                break;
+        }
+    }
+    return {
+        FormValidator: Form_Validator,
+        FormValidatorOnclick: FormValidator_Onclick
+    };
+})();
+
+namespace.Util.Design = {
+    SetColorOnvalidation: function (Result, Element) {
+        var className = Result ? "ValidInput" : "InValidInput";
+        Element.setAttribute("class", className);
+    }
+};
